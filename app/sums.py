864c1bb6ab40f32b4d7bf26707c217f3590daf0a
Kanyesigye Edgar <kedgar751@gmail.com>
2018-09-18 07:23:16 +0300

    write tests for recursive sum

--- app/sums.py
+++ app/sums.py
@@ -1,15 +1,9 @@
-def total_sum(numbers):
+def recursive_sum(numbers):
     total = 0
 
-    index = 0
-    while index < len(numbers):
+    for index in range(len(numbers)):
         if isinstance(numbers[index], list):
-            total += total_sum(numbers[index])
+            total += recursive_sum(numbers[index])
         else:
             total += numbers[index]
-
-        index += 1
     return total
-
-
-print(total_sum([1, 2, 7, 8, [5, 6, 10], 2, [6, 8]]))
---
+++ tests/test_sums.py
@@ -0,0 +1,27 @@
+import unittest
+from app.sums import recursive_sum
+
+
+class TestRecursiveSum(unittest.TestCase):
+    def test_works_for_one_nested_level(self):
+        self.assertEqual(
+            recursive_sum([1, 2, 3, [4, 5, 6]]),
+            21
+        )
+        self.assertEqual(
+            recursive_sum([[4, 5, 6], 1, 2, 3]),
+            21
+        )
+
+        self.assertEqual(
+            recursive_sum([1, 2, 3, [4, 5, 6], 4]),
+            25
+        )
+
+    def test_works_for_two_nested_levels(self):
+        self.assertEqual(recursive_sum(
+            [1, 2, 7, 8, [5, 6, 10], 2, [6, 8]]), 55)
+
+    def test_works_for_3_nested_levels(self):
+        self.assertEqual(recursive_sum(
+            [5, 1, 34, 6, [2, 3, 4, [6, 7, 8, 19], 6, 2, 6, 4], 5, 12]), 130)
